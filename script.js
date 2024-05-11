272cbacef2f3ddb1bdccf704374ad0c78b7b0e59
lautaro <lautaro95ss@gmail.com>
2024-05-11 16:23:24 -0400

    se agrega script para que el navbar aparezca al scrollear hacia abajo

--- script.js
+++ script.js
@@ -61,3 +61,12 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
